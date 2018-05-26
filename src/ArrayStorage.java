c4c29e01f5f733ad6d909908f697583f6da34f0b
DevUniv <devuniv@yandex.ru>
2018-05-26 15:32:23 +0300

    initial commit - hw1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,123 @@
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.SerializationUtils;
+
+import java.io.*;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
+
+    // If save method must only assign elements of in-memory array
+    // (without need for any persistence between JVM runs),
+    // then saveStorageToDisk() shall be commented out.
+
+    //TODO: array initialization now probably not needed,
+    //because constructor reassigns array from loaded file
+    // with 10k array elements
     Resume[] storage = new Resume[10000];
 
-    void clear() {
+    private static final String DISK_STORAGE_FILENAME = "/storage.data";
+
+    public ArrayStorage() {
+        //load prevoiusly saved storage from disk
+        InputStream is = getClass().getResourceAsStream(DISK_STORAGE_FILENAME);
+        // all exceptions are handled internally by Apache Commons method
+        storage = SerializationUtils.deserialize(is);
     }
 
-    void save(Resume r) {
+
+    public void clear() {
+        Arrays.fill(storage, null);
+        saveStorageToDisk();
     }
 
-    Resume get(String uuid) {
-        return null;
+    // TODO: currently it is possible to create Resume with null uuid,
+    // but probably it shall be impossible - change Resume class
+    public void save(Resume resume) {
+        if (resume == null){
+            String msg = "argument cannot be null";
+            throw new IllegalArgumentException(msg);
+        }
+
+        int indexOfFirstNullElement = ArrayUtils.indexOf(storage, null);
+        if (indexOfFirstNullElement < 0) {
+            // TODO: log "storage is out-of-memory",
+            // throw StorageIsFullException
+            // or return boolean from save: true = saved successfully
+            return; // cannot save - no free/null elements in storage array
+        } else {
+            storage[indexOfFirstNullElement] = resume;
+            saveStorageToDisk();
+        }
     }
 
-    void delete(String uuid) {
+
+    public Resume get(String uuid) {
+        if (uuid == null){
+            String msg = "argument cannot be null";
+            throw new IllegalArgumentException(msg);
+        }
+
+        Resume resumeToGet = new Resume();
+        resumeToGet.uuid = uuid;
+        int indexOfResumeMatchingUUID = ArrayUtils.indexOf(storage, resumeToGet);
+        if (indexOfResumeMatchingUUID < 0) {
+            return null; // Resume with such uuid not found
+        } else {
+            return storage[indexOfResumeMatchingUUID];
+        }
+    }
+
+    public void delete(String uuid) {
+        if (uuid == null){
+            String msg = "argument cannot be null";
+            throw new IllegalArgumentException(msg);
+        }
+        // equals + hashCode added to Resume class
+        Resume resumeToDelete = new Resume();
+        resumeToDelete.uuid = uuid;
+        storage = ArrayUtils.removeElement(storage, resumeToDelete);
+        saveStorageToDisk();
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        return new Resume[0];
+    public Resume[] getAll() {
+        int indexOfFirstNullElement = ArrayUtils.indexOf(storage, null);
+        if (indexOfFirstNullElement < 0) {
+            return storage; // no null elements in storage array
+        } else {
+            return Arrays.copyOfRange(storage, 0, indexOfFirstNullElement);
+        }
     }
 
-    int size() {
-        return 0;
+
+    public int size() {
+        int indexOfFirstNullElement = ArrayUtils.indexOf(storage, null);
+        if (indexOfFirstNullElement < 0) {
+            return storage.length; // no null elements in storage array
+        } else {
+            return indexOfFirstNullElement; // number of first non-null elements
+        }
     }
+
+    // saves storage array to resources file
+    private void saveStorageToDisk() {
+        String fileName = getClass().getResource(DISK_STORAGE_FILENAME).getFile();
+        OutputStream os = null;
+        try {
+            os = new BufferedOutputStream(new FileOutputStream(fileName));
+            // all exceptions are handled internally by Apache Commons method
+            SerializationUtils.serialize(storage, os);
+        } catch (FileNotFoundException e) {
+            // TODO: add logging here
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/Resume.java
+++ src/Resume.java
@@ -1,13 +1,46 @@
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import java.io.Serializable;
+
 /**
  * com.urise.webapp.model.Resume class
  */
-public class Resume {
+public class Resume implements Serializable {
 
     // Unique identifier
     String uuid;
+
+
+
 
     @Override
     public String toString() {
         return uuid;
     }
+
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        Resume rhs = (Resume) obj;
+        return new EqualsBuilder()
+                .append(uuid, rhs.uuid)
+                .isEquals();
+    }
+
+
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37).
+                append(uuid).
+                toHashCode();
+    }
+
+
 }
